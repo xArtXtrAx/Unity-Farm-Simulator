ba6b62f4689cef6c393dd53ceaab5d1b85dbc20f
xArtXtrAx <132019928+xArtXtrAx@users.noreply.github.com>
2026-08-05 02:34:07 -0600

    chore: enforce Unity project identity

--- Assets/_Project/Tests/EditMode/ProjectFoundationTests.cs
+++ Assets/_Project/Tests/EditMode/ProjectFoundationTests.cs
@@ -1,7 +1,10 @@
 using System.Linq;
+using FarmSimulator.Application;
 using FarmSimulator.Application.Scenes;
+using FarmSimulator.Editor;
 using NUnit.Framework;
 using UnityEditor;
+using UnityEditor.Build;
 
 namespace FarmSimulator.Tests.EditMode
 {
@@ -18,6 +21,18 @@ namespace FarmSimulator.Tests.EditMode
             Assert.That(enabledScenes, Has.Length.GreaterThanOrEqualTo(2));
             Assert.That(enabledScenes[0], Is.EqualTo(ProjectSceneNames.BootstrapPath));
             Assert.That(enabledScenes[1], Is.EqualTo(ProjectSceneNames.LabPath));
+        }
+
+        [Test]
+        public void ProjectIdentityMatchesRepository()
+        {
+            ProjectIdentityConfigurator.EnsureProjectIdentity();
+
+            Assert.That(PlayerSettings.companyName, Is.EqualTo(ProjectIdentity.CompanyName));
+            Assert.That(PlayerSettings.productName, Is.EqualTo(ProjectIdentity.ProductName));
+            Assert.That(
+                PlayerSettings.GetApplicationIdentifier(NamedBuildTarget.Standalone),
+                Is.EqualTo(ProjectIdentity.StandaloneApplicationIdentifier));
         }
     }
 }
