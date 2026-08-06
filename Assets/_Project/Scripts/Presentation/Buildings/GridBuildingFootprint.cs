518d213ff2bf191a8275927f81682f1501016cdc
xArtXtrAx <132019928+xArtXtrAx@users.noreply.github.com>
2026-08-06 12:06:59 -0600

    feat(buildings): expose occupied grid cells

--- Assets/_Project/Scripts/Presentation/Buildings/GridBuildingFootprint.cs
+++ Assets/_Project/Scripts/Presentation/Buildings/GridBuildingFootprint.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace FarmSimulator.Presentation.Buildings
@@ -28,6 +29,31 @@ namespace FarmSimulator.Presentation.Buildings
         public void SetAnchorCell(Vector2Int cell)
         {
             anchorCell = cell;
+        }
+
+        public IEnumerable<Vector2Int> GetOccupiedCells()
+        {
+            int startX = anchorCell.x - ((gridSize.x - 1) / 2);
+            int startY = anchorCell.y - ((gridSize.y - 1) / 2);
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                for (int x = 0; x < gridSize.x; x++)
+                {
+                    yield return new Vector2Int(startX + x, startY + y);
+                }
+            }
+        }
+
+        public bool Overlaps(GridBuildingFootprint other)
+        {
+            if (other == null || ReferenceEquals(this, other)) return false;
+            var occupied = new HashSet<Vector2Int>(GetOccupiedCells());
+            foreach (Vector2Int cell in other.GetOccupiedCells())
+            {
+                if (occupied.Contains(cell)) return true;
+            }
+
+            return false;
         }
     }
 }
