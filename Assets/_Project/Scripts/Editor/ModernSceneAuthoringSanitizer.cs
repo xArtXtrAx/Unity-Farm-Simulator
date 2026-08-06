ce78bcc895c71b03cbd964f25de1964da808622d
xArtXtrAx <132019928+xArtXtrAx@users.noreply.github.com>
2026-08-06 15:54:53 -0600

    Strip editor-only markers before scene save

---
+++ Assets/_Project/Scripts/Editor/ModernSceneAuthoringSanitizer.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FarmSimulator.Editor
+{
+    [InitializeOnLoad]
+    public static class ModernSceneAuthoringSanitizer
+    {
+        static ModernSceneAuthoringSanitizer()
+        {
+            EditorSceneManager.sceneSaving -= RemoveEditorOnlyMarkers;
+            EditorSceneManager.sceneSaving += RemoveEditorOnlyMarkers;
+        }
+
+        private static void RemoveEditorOnlyMarkers(Scene scene, string path)
+        {
+            foreach (GameObject root in scene.GetRootGameObjects())
+            {
+                ModernSceneAuthoringMarker[] markers =
+                    root.GetComponentsInChildren<ModernSceneAuthoringMarker>(true);
+                foreach (ModernSceneAuthoringMarker marker in markers)
+                {
+                    if (marker != null)
+                    {
+                        Object.DestroyImmediate(marker);
+                    }
+                }
+            }
+        }
+    }
+}
