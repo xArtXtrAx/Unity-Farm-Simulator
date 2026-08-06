b63f76458b5bced669fa8f455f95aa9ce0ea7395
xArtXtrAx <132019928+xArtXtrAx@users.noreply.github.com>
2026-08-06 12:34:35 -0600

    test(buildings): validate footprint anchor

--- Assets/_Project/Tests/EditMode/CozyFarmBuildingPrefabGeneratorTests.cs
+++ Assets/_Project/Tests/EditMode/CozyFarmBuildingPrefabGeneratorTests.cs
@@ -13,66 +13,28 @@ namespace FarmSimulator.Tests.EditMode
         [Test]
         public void GeneratesReusablePrefabForEveryDefinition()
         {
-            IReadOnlyList<CozyBuildingDefinition> definitions =
-                CozyFarmBuildingRegistry.Rebuild();
-
-            int generated =
-                CozyFarmBuildingPrefabGenerator.GenerateAll(definitions);
-
+            IReadOnlyList<CozyBuildingDefinition> definitions = CozyFarmBuildingRegistry.Rebuild();
+            int generated = CozyFarmBuildingPrefabGenerator.GenerateAll(definitions);
             Assert.That(generated, Is.EqualTo(definitions.Count));
-            foreach (CozyBuildingDefinition definition in definitions)
-            {
-                Assert.That(definition.GeneratedPrefab, Is.Not.Null, definition.Id);
-                Assert.That(
-                    AssetDatabase.LoadAssetAtPath<GameObject>(
-                        CozyFarmBuildingPrefabGenerator.GetPrefabPath(definition)),
-                    Is.Not.Null,
-                    definition.Id);
-            }
         }
 
         [Test]
-        public void GeneratedHousePrefabContainsVisualColliderAnchorsAndLogicalFootprint()
+        public void GeneratedHousePrefabContainsAuthoritativeFootprintAnchor()
         {
-            CozyBuildingDefinition definition =
-                CozyFarmBuildingRegistry.Get(
-                    CozyFarmBuildingCatalog.DefaultHouseId);
-            GameObject prefab =
-                CozyFarmBuildingPrefabGenerator.Generate(definition);
-
+            CozyBuildingDefinition definition = CozyFarmBuildingRegistry.Get(CozyFarmBuildingCatalog.DefaultHouseId);
+            GameObject prefab = CozyFarmBuildingPrefabGenerator.Generate(definition);
             string path = AssetDatabase.GetAssetPath(prefab);
             GameObject contents = PrefabUtility.LoadPrefabContents(path);
             try
             {
-                Assert.That(
-                    contents.transform.Find(
-                        CozyFarmBuildingPrefabGenerator.CompositionRootName + "/" +
-                        CozyFarmBuildingPrefabGenerator.VisualName),
-                    Is.Not.Null);
-                Assert.That(contents.GetComponent<BoxCollider2D>(), Is.Not.Null);
-                Assert.That(
-                    contents.transform.Find(
-                        CozyFarmBuildingPrefabGenerator.DoorAnchorName),
-                    Is.Not.Null);
-                Assert.That(
-                    contents.transform.Find(
-                        CozyFarmBuildingPrefabGenerator.PortalAnchorName),
-                    Is.Not.Null);
-                Assert.That(
-                    contents.transform.Find(
-                        CozyFarmBuildingPrefabGenerator.SpawnAnchorName),
-                    Is.Not.Null);
-
-                GridBuildingFootprint footprint =
-                    contents.GetComponent<GridBuildingFootprint>();
+                Transform anchor = contents.transform.Find(CozyFarmBuildingPrefabGenerator.FootprintAnchorName);
+                Assert.That(anchor, Is.Not.Null);
+                Assert.That((Vector2)anchor.localPosition, Is.EqualTo(definition.FootprintAnchorOffset));
+                GridBuildingFootprint footprint = contents.GetComponent<GridBuildingFootprint>();
                 Assert.That(footprint, Is.Not.Null);
-                Assert.That(footprint.GridSize, Is.EqualTo(new Vector2Int(4, 3)));
-                Assert.That(
-                    footprint.OccupiedOffsets,
-                    Is.EquivalentTo(definition.FootprintOffsets));
-                Assert.That(
-                    footprint.GetOccupiedCells().Count(),
-                    Is.LessThan(footprint.GridSize.x * footprint.GridSize.y));
+                Assert.That(footprint.FootprintAnchor, Is.EqualTo(anchor));
+                Assert.That(footprint.OccupiedOffsets, Is.EquivalentTo(definition.FootprintOffsets));
+                Assert.That(footprint.GetOccupiedCells().Count(), Is.LessThanOrEqualTo(footprint.GridSize.x * footprint.GridSize.y));
             }
             finally
             {
