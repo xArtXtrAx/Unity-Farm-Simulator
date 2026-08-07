205b8ac768dc3b6c90cb062a46b40baaa25011e2
xArtXtrAx <132019928+xArtXtrAx@users.noreply.github.com>
2026-08-06 19:38:20 -0600

    Update item catalog tests for potato and radish

--- Assets/_Project/Tests/EditMode/ItemCatalogTests.cs
+++ Assets/_Project/Tests/EditMode/ItemCatalogTests.cs
@@ -11,20 +11,11 @@ namespace FarmSimulator.Tests.EditMode
         [Test]
         public void CatalogContainsExactlyTheEightApprovedItems()
         {
-            string[] ids = ItemCatalog.All
-                .Select(definition => definition.Id.Value)
-                .ToArray();
-
+            string[] ids = ItemCatalog.All.Select(definition => definition.Id.Value).ToArray();
             Assert.That(ids, Is.EqualTo(new[]
             {
-                "hoe",
-                "watering-can",
-                "turnip-seeds",
-                "turnip",
-                "carrot-seeds",
-                "carrot",
-                "cabbage-seeds",
-                "cabbage"
+                "hoe", "watering-can", "turnip-seeds", "turnip",
+                "potato-seeds", "potato", "radish-seeds", "radish"
             }));
         }
 
@@ -32,21 +23,16 @@ namespace FarmSimulator.Tests.EditMode
         [TestCase("watering-can", "Regadera", "RG", ItemCategory.Tool, 1, 0, 0)]
         [TestCase("turnip-seeds", "Semillas de nabo", "SN", ItemCategory.Seed, 99, 30, 0)]
         [TestCase("turnip", "Nabo", "NB", ItemCategory.Crop, 99, 0, 60)]
-        [TestCase("carrot-seeds", "Semillas de zanahoria", "SZ", ItemCategory.Seed, 99, 50, 0)]
-        [TestCase("carrot", "Zanahoria", "ZN", ItemCategory.Crop, 99, 0, 100)]
-        [TestCase("cabbage-seeds", "Semillas de col", "SC", ItemCategory.Seed, 99, 75, 0)]
-        [TestCase("cabbage", "Col", "CL", ItemCategory.Crop, 99, 0, 150)]
+        [TestCase("potato-seeds", "Semillas de papa", "SP", ItemCategory.Seed, 99, 50, 0)]
+        [TestCase("potato", "Papa", "PP", ItemCategory.Crop, 99, 0, 100)]
+        [TestCase("radish-seeds", "Semillas de rábano", "SR", ItemCategory.Seed, 99, 75, 0)]
+        [TestCase("radish", "Rábano", "RB", ItemCategory.Crop, 99, 0, 150)]
         public void DefinitionMatchesApprovedSourceData(
-            string itemId,
-            string expectedName,
-            string expectedShortLabel,
-            ItemCategory expectedCategory,
-            int expectedStackLimit,
-            int expectedBuyPrice,
-            int expectedSellPrice)
+            string itemId, string expectedName, string expectedShortLabel,
+            ItemCategory expectedCategory, int expectedStackLimit,
+            int expectedBuyPrice, int expectedSellPrice)
         {
             ItemDefinition definition = ItemCatalog.Get(itemId);
-
             Assert.That(definition.Id.Value, Is.EqualTo(itemId));
             Assert.That(definition.Name, Is.EqualTo(expectedName));
             Assert.That(definition.ShortLabel, Is.EqualTo(expectedShortLabel));
@@ -59,6 +45,8 @@ namespace FarmSimulator.Tests.EditMode
         [TestCase(null)]
         [TestCase("")]
         [TestCase("unknown")]
+        [TestCase("carrot")]
+        [TestCase("cabbage")]
         public void UnknownStringIdsAreRejectedExplicitly(string itemId)
         {
             Assert.That(ItemCatalog.TryGet(itemId, out _), Is.False);
@@ -78,16 +66,11 @@ namespace FarmSimulator.Tests.EditMode
         [Test]
         public void OnlySpringSeedsArePurchasable()
         {
-            string[] purchasable = ItemCatalog.All
-                .Where(definition => definition.IsPurchasable)
-                .Select(definition => definition.Id.Value)
-                .ToArray();
-
+            string[] purchasable = ItemCatalog.All.Where(definition => definition.IsPurchasable)
+                .Select(definition => definition.Id.Value).ToArray();
             Assert.That(purchasable, Is.EqualTo(new[]
             {
-                "turnip-seeds",
-                "carrot-seeds",
-                "cabbage-seeds"
+                "turnip-seeds", "potato-seeds", "radish-seeds"
             }));
             Assert.That(ItemCatalog.IsPurchasable("unknown"), Is.False);
             Assert.That(ItemCatalog.IsPurchasable(null), Is.False);
@@ -96,17 +79,9 @@ namespace FarmSimulator.Tests.EditMode
         [Test]
         public void OnlySpringCropsAreSellable()
         {
-            string[] sellable = ItemCatalog.All
-                .Where(definition => definition.IsSellable)
-                .Select(definition => definition.Id.Value)
-                .ToArray();
-
-            Assert.That(sellable, Is.EqualTo(new[]
-            {
-                "turnip",
-                "carrot",
-                "cabbage"
-            }));
+            string[] sellable = ItemCatalog.All.Where(definition => definition.IsSellable)
+                .Select(definition => definition.Id.Value).ToArray();
+            Assert.That(sellable, Is.EqualTo(new[] { "turnip", "potato", "radish" }));
             Assert.That(ItemCatalog.IsSellable("unknown"), Is.False);
             Assert.That(ItemCatalog.IsSellable(null), Is.False);
         }
@@ -115,7 +90,6 @@ namespace FarmSimulator.Tests.EditMode
         public void CatalogCollectionCannotBeMutatedExternally()
         {
             var list = (IList<ItemDefinition>)ItemCatalog.All;
-
             Assert.Throws<NotSupportedException>(() => list.RemoveAt(0));
             Assert.That(ItemCatalog.All, Has.Count.EqualTo(8));
         }
@@ -123,17 +97,11 @@ namespace FarmSimulator.Tests.EditMode
         [Test]
         public void CategoryEnumPreservesAllKnownSourceCategories()
         {
-            Assert.That(
-                Enum.GetValues(typeof(ItemCategory)).Cast<ItemCategory>(),
-                Is.EqualTo(new[]
-                {
-                    ItemCategory.Tool,
-                    ItemCategory.Seed,
-                    ItemCategory.Crop,
-                    ItemCategory.Material,
-                    ItemCategory.Food,
-                    ItemCategory.Gift
-                }));
+            Assert.That(Enum.GetValues(typeof(ItemCategory)).Cast<ItemCategory>(), Is.EqualTo(new[]
+            {
+                ItemCategory.Tool, ItemCategory.Seed, ItemCategory.Crop,
+                ItemCategory.Material, ItemCategory.Food, ItemCategory.Gift
+            }));
         }
 
         [Test]
@@ -145,69 +113,40 @@ namespace FarmSimulator.Tests.EditMode
         }
 
         [Test]
-        public void DefinitionRejectsUnregisteredId()
-        {
+        public void DefinitionRejectsUnregisteredId() =>
             Assert.Throws<ArgumentOutOfRangeException>(() =>
                 new ItemDefinition(default(ItemId), "Objeto", "OB", ItemCategory.Material, 1));
-        }
 
         [Test]
-        public void DefinitionRejectsBlankName()
-        {
+        public void DefinitionRejectsBlankName() =>
             Assert.Throws<ArgumentException>(() =>
                 new ItemDefinition(ItemId.Hoe, " ", "AZ", ItemCategory.Tool, 1));
-        }
 
         [Test]
-        public void DefinitionRejectsBlankShortLabel()
-        {
+        public void DefinitionRejectsBlankShortLabel() =>
             Assert.Throws<ArgumentException>(() =>
                 new ItemDefinition(ItemId.Hoe, "Azada", " ", ItemCategory.Tool, 1));
-        }
 
         [Test]
-        public void DefinitionRejectsUnknownCategory()
-        {
+        public void DefinitionRejectsUnknownCategory() =>
             Assert.Throws<ArgumentOutOfRangeException>(() =>
                 new ItemDefinition(ItemId.Hoe, "Azada", "AZ", (ItemCategory)99, 1));
-        }
 
         [Test]
-        public void DefinitionRejectsNonPositiveStackLimit()
-        {
+        public void DefinitionRejectsNonPositiveStackLimit() =>
             Assert.Throws<ArgumentOutOfRangeException>(() =>
                 new ItemDefinition(ItemId.Hoe, "Azada", "AZ", ItemCategory.Tool, 0));
-        }
 
         [Test]
-        public void DefinitionRejectsNonPositiveBuyPrice()
-        {
+        public void DefinitionRejectsNonPositiveBuyPrice() =>
             Assert.Throws<ArgumentOutOfRangeException>(() =>
-                new ItemDefinition(
-                    ItemId.TurnipSeeds,
-                    "Semillas de nabo",
-                    "SN",
-                    ItemCategory.Seed,
-                    99,
-                    buyPrice: 0));
-        }
+                new ItemDefinition(ItemId.TurnipSeeds, "Semillas de nabo", "SN", ItemCategory.Seed, 99, buyPrice: 0));
 
         [Test]
-        public void DefinitionRejectsNonPositiveSellPrice()
-        {
+        public void DefinitionRejectsNonPositiveSellPrice() =>
             Assert.Throws<ArgumentOutOfRangeException>(() =>
-                new ItemDefinition(
-                    ItemId.Turnip,
-                    "Nabo",
-                    "NB",
-                    ItemCategory.Crop,
-                    99,
-                    sellPrice: 0));
-        }
+                new ItemDefinition(ItemId.Turnip, "Nabo", "NB", ItemCategory.Crop, 99, sellPrice: 0));
 
-        private static int? ToOptionalPrice(int price)
-        {
-            return price == 0 ? (int?)null : price;
-        }
+        private static int? ToOptionalPrice(int price) => price == 0 ? (int?)null : price;
     }
 }
