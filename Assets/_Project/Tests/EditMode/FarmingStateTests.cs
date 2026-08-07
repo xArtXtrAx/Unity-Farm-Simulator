158e6eabd339e2e182b9e62485733c0388cf23fb
xArtXtrAx <132019928+xArtXtrAx@users.noreply.github.com>
2026-08-06 19:49:50 -0600

    Cover full farming domain loop

--- Assets/_Project/Tests/EditMode/FarmingStateTests.cs
+++ Assets/_Project/Tests/EditMode/FarmingStateTests.cs
@@ -1,4 +1,5 @@
 using FarmSimulator.Domain.Farming;
+using FarmSimulator.Domain.Inventory;
 using FarmSimulator.Domain.Items;
 using NUnit.Framework;
 
@@ -45,24 +46,49 @@ namespace FarmSimulator.Tests.EditMode
             Assert.That(plot.IsMature, Is.False);
         }
 
-        [Test]
-        public void MatureCropHarvestsAndLeavesTilledSoil()
+        [TestCase("turnip-seeds", 5, 2)]
+        [TestCase("potato-seeds", 6, 3)]
+        [TestCase("radish-seeds", 5, 4)]
+        public void CropUsesItsRealVisualStageCount(string seedId, int expectedStages, int expectedDays)
         {
+            CropDefinition crop = CropCatalog.GetBySeed(ItemId.Parse(seedId));
+            Assert.That(crop.VisualStageCount, Is.EqualTo(expectedStages));
+            Assert.That(crop.FinalVisualStage, Is.EqualTo(expectedStages - 1));
+            Assert.That(crop.DaysToMature, Is.EqualTo(expectedDays));
+            Assert.That(crop.GetVisualStage(0), Is.Zero);
+            Assert.That(crop.GetVisualStage(expectedDays), Is.EqualTo(expectedStages - 1));
+        }
+
+        [TestCase("turnip-seeds", "turnip")]
+        [TestCase("potato-seeds", "potato")]
+        [TestCase("radish-seeds", "radish")]
+        public void CompleteLoopHarvestsEachCropIntoInventory(string seedId, string harvestId)
+        {
+            ItemId seed = ItemId.Parse(seedId);
+            ItemId harvest = ItemId.Parse(harvestId);
+            CropDefinition crop = CropCatalog.GetBySeed(seed);
             var plot = new FarmPlotState();
-            plot.Till();
-            plot.Plant(ItemId.TurnipSeeds);
-            for (int day = 0; day < 2; day++)
+            var inventory = new InventoryState(new[] { new InventorySlotSnapshot(seed.Value, 1) });
+
+            Assert.That(plot.Till(), Is.True);
+            Assert.That(plot.Plant(seed), Is.True);
+            Assert.That(inventory.ConsumeSelected(), Is.True);
+
+            for (int day = 0; day < crop.DaysToMature; day++)
             {
-                plot.Water();
-                plot.AdvanceDay();
+                Assert.That(plot.Water(), Is.True);
+                Assert.That(plot.AdvanceDay(), Is.True);
             }
+
             Assert.That(plot.IsMature, Is.True);
-            Assert.That(plot.VisualStage, Is.EqualTo(CropCatalog.FinalVisualStage));
-            Assert.That(plot.TryHarvest(out ItemId crop), Is.True);
-            Assert.That(crop, Is.EqualTo(ItemId.Turnip));
+            Assert.That(plot.VisualStage, Is.EqualTo(crop.FinalVisualStage));
+            Assert.That(plot.TryHarvest(out ItemId harvested), Is.True);
+            Assert.That(harvested, Is.EqualTo(harvest));
+            Assert.That(inventory.AddItem(harvested).Changed, Is.True);
+            Assert.That(inventory.GetSlot(0).ItemId, Is.EqualTo(harvest));
+            Assert.That(inventory.GetSlot(0).Quantity, Is.EqualTo(1));
             Assert.That(plot.HasCrop, Is.False);
             Assert.That(plot.IsTilled, Is.True);
-            Assert.That(plot.IsWatered, Is.False);
         }
 
         [Test]
@@ -84,11 +110,22 @@ namespace FarmSimulator.Tests.EditMode
         }
 
         [Test]
-        public void CropCatalogMapsAllExistingSeeds()
+        public void CropCatalogMapsAllFinalSeeds()
         {
             Assert.That(CropCatalog.GetBySeed(ItemId.TurnipSeeds).HarvestItemId, Is.EqualTo(ItemId.Turnip));
             Assert.That(CropCatalog.GetBySeed(ItemId.PotatoSeeds).HarvestItemId, Is.EqualTo(ItemId.Potato));
             Assert.That(CropCatalog.GetBySeed(ItemId.RadishSeeds).HarvestItemId, Is.EqualTo(ItemId.Radish));
+        }
+
+        [Test]
+        public void InitialHotbarContainsToolsAndAllFinalSeeds()
+        {
+            InventoryState inventory = InventoryState.CreateInitialPlayerInventory();
+            Assert.That(inventory.GetSlot(0).ItemId, Is.EqualTo(ItemId.Hoe));
+            Assert.That(inventory.GetSlot(1).ItemId, Is.EqualTo(ItemId.WateringCan));
+            Assert.That(inventory.GetSlot(2).ItemId, Is.EqualTo(ItemId.TurnipSeeds));
+            Assert.That(inventory.GetSlot(3).ItemId, Is.EqualTo(ItemId.PotatoSeeds));
+            Assert.That(inventory.GetSlot(4).ItemId, Is.EqualTo(ItemId.RadishSeeds));
         }
     }
 }
